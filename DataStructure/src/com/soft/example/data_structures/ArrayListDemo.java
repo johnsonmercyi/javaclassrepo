1087d6d617f9827c045fbb166de68fc12799d81a
Cletus Ori <cletusori@Cletuss-MacBook-Air.local>
2023-10-10 15:53:35 -0300

    assignment for review

--- DataStructure/src/com/soft/example/data_structures/ArrayListDemo.java
+++ DataStructure/src/com/soft/example/data_structures/ArrayListDemo.java
@@ -52,6 +52,63 @@ public class ArrayListDemo {
   }
 
   //get many persons from the list = getPersons()
+  /**
+   * @param index
+   * @return
+   */
+  public ArrayList<Person> getPersons(int ...index){
+    if(index.length == 0){
+      return null;
+    }
+    int indexHolder[] = new int[index.length];
+    for(int i=0; i < index.length; i++){
+      if(persons.size() - 1 >= index[i]){
+        indexHolder[i] = index[i];
+      }else{
+        indexHolder[i] = -1;
+      }
+    }
+
+    ArrayList<Person> holdPersons = new ArrayList<>();
+    for(int i=0; i < indexHolder.length; i++){
+      if(indexHolder.length-1 > indexHolder[i] && indexHolder[i] != -1){ 
+        
+        if(persons.get(indexHolder[i]) != null){
+          Person p = persons.get(indexHolder[i]);
+          //holdPersons.set(i, p);
+          holdPersons.add(p);
+          System.out.println("index: "+ indexHolder[i]+ " - " + p);
+        }
+      } else{
+        System.out.println(indexHolder[i]+" does not exits");
+      } 
+    }
+    return holdPersons;
+  }
+
+  //converting person's arraylist to person array
+  public Person[] arrayListToArray(ArrayList<Person> p){
+    Person personsArray[] = new Person[p.size()];
+    for(int i = 0; i < p.size(); i++){
+      personsArray[i] = p.get(i);
+    }
+    return personsArray;
+  }
+
+  //Method to return length of an arraylist
+  public int getPersonLength(ArrayList<Person> p){
+    return p.size();
+  }
+
+  //method to set arraylist using index number
+  public Person setPerson(int index, Person p){
+    return persons.set(index, p);
+  }
+
+  //method to remove all elements in the arraylist
+  public void clearPersonlist(ArrayList<Person> p){
+    p.clear();
+  }
 
   public static void main(String[] args) {
     ArrayListDemo demo = new ArrayListDemo();
@@ -62,6 +119,8 @@ public class ArrayListDemo {
     Person mary = new Person("Mary Lee", "123456789", 'F', new Date());
 
     demo.addPerson(john, jane, mary, null);
+    demo.getPersons(1,2,3,4,0,5,6,0);
+                              
 
   }
 }
