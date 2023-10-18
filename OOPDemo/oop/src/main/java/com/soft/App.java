d19259700a60ff13585bf0cc3d2a25fcb11b81a2
Cletus Ori <cletusori@Cletuss-MacBook-Air.local>
2023-10-18 19:57:52 -0300

    latest code on recursion assignment

--- OOPDemo/oop/src/main/java/com/soft/App.java
+++ OOPDemo/oop/src/main/java/com/soft/App.java
@@ -6,6 +6,18 @@ import com.soft.inheritance.Dog;
 import com.soft.inheritance.GermanShepherd;
 
 public class App {
+
+  static class FibonacciCalculator {
+  
+    public static long calculateFibonacci(int n) {
+        if (n <= 1) {
+            return n; // Base case: Fibonacci of 0 is 0, and Fibonacci of 1 is 1.
+        } else {
+            return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
+        }
+    }
+  }
+
   public static void main(String[] args) {
     Animal animal = new Animal();
     Animal dog = new Dog();
@@ -18,5 +30,11 @@ public class App {
     // System.out.println(cat.getName());
     // System.out.println(dog.getName());
     System.out.println(animal);
+
+    System.out.println("\nRecursion Asignment\n");
+    int n = 10; // Calculate the 10th Fibonacci number
+    long result = App.FibonacciCalculator.calculateFibonacci(n);
+    System.out.println("The " + n + "th Fibonacci number is: " + result);
+    
   }
 }
