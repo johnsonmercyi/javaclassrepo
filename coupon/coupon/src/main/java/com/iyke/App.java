4cc3ddfcf2a27322712d512aaf77d56e780378be
iykeori <oricletus@gmail.com>
2023-10-23 13:34:22 -0300

    last-update

--- coupon/coupon/src/main/java/com/iyke/App.java
+++ coupon/coupon/src/main/java/com/iyke/App.java
@@ -8,11 +8,10 @@ public class App {
     CouponService cService = new CouponService();
     
     //Testing the READ in the CRUD
-    //Getting all coupons from the DB and assigning the to an Arraylist variable dbCoupons
     ArrayList<Coupon> dbCoupons = cService.getCoupons();
-    System.out.println("\n All Coupons Information \n");
-    
 
+    System.out.println("\n All Coupons Test \n");
+ 
     //Testing Reading/ getting couon using id
     System.out.println("\nGet Coupon with ID: " + cService.getCoupon(dbCoupons.get(3).getId()));
 
@@ -25,18 +24,13 @@ public class App {
     // Testing the Deleting by id in the CRUD and displaying deletedcoupon
     System.out.println("\nDeleted Coupon by code name: " + cService.deleteCoupon("love"));
 
-    System.out.println("\n\n");
-    // System.out.println("\nDatabase Coupons: " + dbCoupons);
-
-    //Testing Create Coupon in CRUD
     System.out.println("\nCreate Couupon: " + cService.createCoupon(new Coupon("test-coupon", 3.44, LocalDate.of(2023, 9, 15))));
     System.out.println("\n\n");
     // System.out.println("\nDatabase Coupons: " + dbCoupons);
 
     // Testing the Modify coupon in the CRUD
     System.out.println("\nmodified Coupon: " + cService.modifyCoupon(new Coupon("testcode", 5.0, LocalDate.of(2023, 03, 23))));
-    
-    //Print All coupons
+  
     System.out.println("\nDatabase Coupons: " + dbCoupons);
   }
 
